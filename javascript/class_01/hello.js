c1d105d0c9d828ae330c09d5bcefde31b0d5a89f
toukir <toukirahmed.th@gmail.com>
2024-11-19 10:31:46 +0600

    change java script file

---
+++ javascript/class_01/hello.js
@@ -0,0 +1,9 @@
+console.log(10+20);
+console.log("ami programmer");
+console.log("i am toukir ahmed abid");
+
+var a=50;
+var b=20;
+console.log(a+b);
+ var myName="toukir ahmed abid";
+ console.log(myName);
