465c2a322203eeeaf627300974c0d43d6795b216
toukir <toukirahmed.th@gmail.com>
2024-11-24 23:22:28 +0600

    last update

---
+++ javascript/DOM-learning/first.js
@@ -0,0 +1,14 @@
+
+const heading=document.getElementById('heading_01');
+heading.style.color='red';
+heading.style.fontSize='24px';
+heading.style.backgroundColor='green';
+heading.style.textAlign='center';
+console.log(heading);
+
+const body=document.getElementById('body_tag');
+
+const jsdiv= document.createElement('div');
+body.appendChild(div);
+
+jsdiv
