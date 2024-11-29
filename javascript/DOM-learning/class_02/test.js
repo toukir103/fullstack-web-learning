09a6cb806cf9e3be4dfe70238f8f8f6b804eea15
Toukir Ahmed Abid <toukirahmed.th@gmail.com>
2024-11-29 19:48:37 +0600

    Updated index.html and test.js for DOM learning

--- javascript/DOM-learning/class_02/test.js
+++ javascript/DOM-learning/class_02/test.js
@@ -1,5 +1,20 @@
-alert("I am Abid")
-
-function changeColor(){
-    document.getElementById('body_tag').style.backgroundColor ='red';
+function loadApiData() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+.then(response => response.json())
+.then(data => displayData(data))
 }
+function displayData(data) {
+     const div =document.getElementById('apiData');
+
+     data.map(user =>
+          {
+            const p= document.createElement('p');
+            p.innerText= user.email;
+            div.appendChild(p);
+          }
+
+
+     )
+    
+}
+
